be5b545aa465ca39a48c2831605424970fe44271
joshipj22 <piyushinbox1@gmail.com>
2026-04-27 23:41:57 +0530

    pytest usage

---
+++ test_square.py
@@ -0,0 +1,21 @@
+from square import get_square
+
+def test_positive():
+    x=5
+    expected=25
+    res=get_square(x)
+    assert res==expected
+
+
+def test_one():
+    x=1
+    expected=1
+    res=get_square(x)
+    assert res==expected
+
+
+def test_negitive():
+    x=-1
+    expected=1
+    res=get_square(x)
+    assert res==expected
